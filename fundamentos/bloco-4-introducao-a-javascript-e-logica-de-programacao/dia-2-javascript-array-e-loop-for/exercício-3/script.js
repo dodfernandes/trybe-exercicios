9f48e5b4250481196314345bced17117b9cd1803
Douglas Fernandes <fernandes.dod@gmail.com>
2022-05-13 18:59:00 -0300

    Bloco4.2 - exercício 03

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercício-3/script.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let media = 0
+
+for (let index = 0; index < numbers.length; index += 1) {
+    media += numbers[index];
+};
+
+media = media / 2;
+
+console.log('A média aritmética dos valores contidos no array é=', media);
